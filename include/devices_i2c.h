201badcfaa8d074e508e56810da2ffae4efe53bd
Manfred Mueller-Spaeth <fms1961@gmail.com>
2018-06-13 17:05:44 +0200

    Blinking added

--- include/devices_i2c.h
+++ include/devices_i2c.h
@@ -4,9 +4,16 @@
 #include "mgos.h"
 #include "mgos_app.h"
 #include "mgos_i2c.h"
+#include "mgos_timers.h"
 #include "esp32_hw_timers.h"
 
+#define MODE_SEQ   0
+#define MODE_BLINK 1
+
 struct device_ctrl {
+	int addr;
+	uint8_t mode;
+	uint8_t bitmask;
 	uint8_t last;
 	uint8_t curr;
 	uint8_t steps;
@@ -15,15 +22,17 @@ struct device_ctrl {
 	uint8_t green;
 	uint16_t delay;
 	uint8_t *seq;
-	int addr;
+	mgos_timer_id timerId;
 	struct mgos_i2c *i2c;
 };
 
 bool mgos_devices_i2c_init(void);
 
 void set_device_i2c(uint16_t addr, uint8_t pins);
-void test_lamp(int red, int yellow, int green, int last, int delay, int addr);
+void *test_lamp(int red, int yellow, int green, int delay, int addr, int mask);
+void *blink_lamp(int pins, int delay, int addr, int mask);
 
+IRAM void handleSequence(struct device_ctrl *currDevice);
 IRAM void _set_device_i2c(struct device_ctrl *currDevice);
 IRAM void device_cb(void *arg);
 
--- src/devices_i2c.c
+++ src/devices_i2c.c
@@ -5,11 +5,18 @@
  * Hardware timer functions
  * 
  */
-void set_device_i2c(uint16_t addr, uint8_t pins) {
-
+void set_device_i2c(uint16_t addr, uint8_t pins, void *oldDevice) {
+	
+	struct device_ctrl *currDevice = (struct device_ctrl *) oldDevice;
   static uint8_t currPins;
-	struct mgos_i2c *i2c = mgos_i2c_get_global();
+	struct mgos_i2c *i2c;
+	
+	// if a timer is active, we stop it
+	if (currDevice->timerId !== null) {
+		mgos_clear_hw_timer(currDevice->timerId);
+	}
 
+	i2c = mgos_i2c_get_global();
   if (i2c == NULL) {
     LOG(LL_DEBUG, ("I2C is disabled - error: %d", 503));
   } else {
@@ -26,24 +33,7 @@ void set_device_i2c(uint16_t addr, uint8_t pins) {
   }
 }
 
-IRAM void _set_device_i2c(struct device_ctrl *currDevice) {
-
-  static uint8_t currPins;
-  static uint8_t pins;
-  
-  if (currDevice->i2c != NULL) {
- 		if (mgos_i2c_read(currDevice->i2c, currDevice->addr, &currPins, 1, true)) {
-		 	pins = (currPins & 0xF8) | currDevice->seq[currDevice->curr];
-		  mgos_i2c_write(currDevice->i2c, currDevice->addr, &pins, 1, true);
- 		}
-  }
-  
-}
-
-IRAM void device_cb(void *arg) {
-
-  struct device_ctrl *currDevice = (struct device_ctrl *) arg;;
-  
+IRAM void handleSequence(struct device_ctrl *currDevice) {
 	if (currDevice->curr >= currDevice->steps) {
 		if (currDevice->addr == -1) {
 			mgos_gpio_write(currDevice->red, 0);
@@ -53,18 +43,15 @@ IRAM void device_cb(void *arg) {
 			currDevice->curr = 0;
 			_set_device_i2c(currDevice);
 		}
-	 	(void) arg;
+		(void) arg;
 		return;
 	}
-
 	if (currDevice->addr == -1) {
 		if (currDevice->curr > 0) {
 			mgos_gpio_write(currDevice->seq[currDevice->curr], 0);
 		}
 	}
-
 	currDevice->curr++;
-
 	if (currDevice->curr < currDevice->steps) {
 		if (currDevice->addr == -1) {
 			if (currDevice->seq[currDevice->curr] > 0) {
@@ -73,26 +60,61 @@ IRAM void device_cb(void *arg) {
 		} else {
 			_set_device_i2c(currDevice);
 		}
-		mgos_set_hw_timer(1000 * currDevice->delay, MGOS_ESP32_HW_TIMER_IRAM, device_cb, currDevice); 
+		currDevice->timerId = mgos_set_hw_timer(1000 * currDevice->delay, MGOS_ESP32_HW_TIMER_IRAM, device_cb, currDevice); 
 	}
+
+}
+
+IRAM void handleBlink(struct device_ctrl *currDevice) {
+	_set_device_i2c(currDevice);
+	currDevice->last = (currDevice->last == 1) ? 0 : 1;
+	currDevice->timerID = mgos_set_hw_timer(1000 * currDevice->delay, MGOS_ESP32_HW_TIMER_IRAM, device_cb, currDevice); 
+}
+
+IRAM void _set_device_i2c(struct device_ctrl *currDevice) {
+
+  static uint8_t currPins;
+  static uint8_t pins;
+  static uint8_t newPins = currDevice->seq[currDevice->curr];
+  
+  if (currDevice->i2c != NULL) {
+ 		if (mgos_i2c_read(currDevice->i2c, currDevice->addr, &currPins, 1, true)) {
+			if (currDevice->mode == MODE_BLINK) {
+				newPins = currDevice->mask & (currDevice->last == 0 ? ~currDevice->curr : currDevice->curr);
+		  } 
+		 	pins = (currPins & ~currDevice->mask) | (newPins & currDevice->mask);
+		  mgos_i2c_write(currDevice->i2c, currDevice->addr, &pins, 1, true);
+ 		}
+  }
+}
+
+IRAM void device_cb(void *arg) {
+
+  struct device_ctrl *currDevice = (struct device_ctrl *) arg;;
+  
+  switch (currDevice->mode) {
+  	case MODE_BLINK:
+			handleBlink(currDevice);
+  	break;
+  	case MODE_SEQ:
+  	default:
+  		handleSequence(currDevice);
+  	break;
+  }
 
  	(void) arg;
 }
- 
-void test_lamp(int red, int yellow, int green, int last, int delay, int addr) {
+
+void *blink_lamp(int pin, int delay, int addr, int mask) {
 
 	static struct device_ctrl currDevice;
 
 	struct mgos_i2c *myI2C = mgos_i2c_get_global();
-  static uint8_t seq[] = { 0, 1, 2, 4, 0 };
   
-  currDevice.curr = 0;
-  currDevice.steps = 5;
-  currDevice.seq = seq;
-  currDevice.red = red;
-  currDevice.yellow = yellow;
-  currDevice.green = green;
-  currDevice.last = last;
+  currDevice.mode = MODE_BLINK;
+  currDevice.mask = mask;
+  currDevice.curr = pin;
+  currDevice.last = 0;
   currDevice.delay = delay;
   currDevice.addr = addr;
   currDevice.i2c = myI2C;
@@ -110,7 +132,52 @@ void test_lamp(int red, int yellow, int green, int last, int delay, int addr) {
  	} else {
 	 	_set_device_i2c(&currDevice);
  	}
-	mgos_set_hw_timer(1000 * currDevice.delay,  MGOS_ESP32_HW_TIMER_IRAM, device_cb, &currDevice ); 
+	currDevice.timerID = mgos_set_hw_timer(1000 * currDevice.delay, MGOS_ESP32_HW_TIMER_IRAM, device_cb, &currDevice); 
+	return &currDevice;
+}
+
+void *test_lamp(int red, int yellow, int green, int delay, int addr, int mask) {
+
+	static struct device_ctrl currDevice;
+
+	struct mgos_i2c *myI2C = mgos_i2c_get_global();
+  static uint8_t seqI2C[5] = { 0, 1, 2, 4, 0 };
+  static uint8_t seqGPIO[5];
+  
+  seqGPIO[0] = 0;
+  seqGPIO[1] = red;
+  seqGPIO[2] = yellow;
+  seqGPIO[3] = green;
+  seqGPIO[4] = 0;
+
+  currDevice.mode = MODE_SEQ;
+  currDevice.curr = 0;
+  currDevice.last = 0;
+  currDevice.steps = 5;
+  currDevice.seq = (addr == -1) ? seqGPIO : seqI2C;
+  currDevice.red = red;
+  currDevice.yellow = yellow;
+  currDevice.green = green;
+  currDevice.mask = mask;
+  currDevice.delay = delay;
+  currDevice.addr = addr;
+  currDevice.i2c = myI2C;
+ 
+ 	if (currDevice.addr == -1) {
+		mgos_gpio_set_mode(red, MGOS_GPIO_MODE_OUTPUT);
+		mgos_gpio_set_pull(red, MGOS_GPIO_PULL_DOWN);
+		mgos_gpio_write(red, 0);
+		mgos_gpio_set_mode(yellow, MGOS_GPIO_MODE_OUTPUT);
+		mgos_gpio_set_pull(yellow, MGOS_GPIO_PULL_DOWN);
+		mgos_gpio_write(yellow, 0);
+		mgos_gpio_set_mode(green, MGOS_GPIO_MODE_OUTPUT);
+		mgos_gpio_set_pull(green, MGOS_GPIO_PULL_DOWN);
+		mgos_gpio_write(green, 0);
+ 	} else {
+	 	_set_device_i2c(&currDevice);
+ 	}
+	currDevice.timerID = mgos_set_hw_timer(1000 * currDevice.delay,  MGOS_ESP32_HW_TIMER_IRAM, device_cb, &currDevice ); 
+	return &currDevice;
 }
 
 bool mgos_devices_i2c_init(void) {
